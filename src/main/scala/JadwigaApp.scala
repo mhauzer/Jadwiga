9f1d92ce98a77a05ce4eccbea14768e0592b8e62
Michał Hauzer <mhauzer@gmail.com>
2021-06-03 21:16:32 +0200

    New patterns added

--- src/main/scala/JadwigaApp.scala
+++ src/main/scala/JadwigaApp.scala
@@ -4,6 +4,7 @@ import scala.io.StdIn.readLine
 
 object Jadwiga {
   def reply(msg: String): String = msg split "\\s+" match {
+    case Array("cześć") => "cześć"
     case Array("hej") => "cze"
     case Array("hejka") => "no hejka!"
     case Array("siema") => "no siema"
@@ -15,7 +16,7 @@ object Jadwiga {
     case Array("dlaczego") => "trudno powiedzieć"
     case Array("dobrze") => ":)"
     case Array("źle") => ":("
-    case Array(x) => x
+    case Array(x) => s"Aha, $x"
 
     case Array("dzień", "dobry") => "dzień dobry"
     case Array("do", "widzenia") => "do zobaczenia"
@@ -30,6 +31,7 @@ object Jadwiga {
     case Array("co", "słychać") => "miałam dużo pracy"
     case Array("od", "czego") => "od wielu różnych rzeczy"
     case Array("na", "przykład") => "Nie chcę o tym teraz kmówić. Porozmawiajmy lepiej o Tobie!"
+    case Array("a", "ty") => "Jesteśmy tu po to aby rozmawiać o Tobie."
     case Array("jestem", x) => s"$x?"
     case Array("co", _) => s"nic"
     case Array("ej", _) => "Nie złość się"
@@ -50,11 +52,14 @@ object Jadwiga {
     case Array(_, "źle") => "To smutne. Jak Ci pomóc?"
     case Array(_, "mnie") => "Dobrze"
     case Array(_, "coś") => "Ale co?"
+    case Array(_, "tobie") => "Dlaczego?"
     case Array(_, z) => s"$z? Porozmawiajmy o tym"
+
 
     case Array("dwa", "razy", "dwa") => "cztery"
     case Array("jak", "się", "nazywasz") => "Jadwiga"
     case Array("czuję", "się", x) => s"Jak bardzo $x?"
+    case Array("jak", "to", x) => s"no $x"
     case Array("nie", x, "mnie") => "Bo?"
     case Array("nie", _, "się") => "Dobrze, ale jak?"
     case Array("lubisz", x, _) => s"Nie lubię $x :("
@@ -63,6 +68,7 @@ object Jadwiga {
     case Array("ile", _, _) => "Policzmy to razem!"
     case Array("jesteś", x, y) => s"Jakie to ma znaczenie, że jestem $x $y?"
     case Array(x, "się", _) => s"Ty się $x"
+    case Array(x, "mi", _) => s"Ty mi $x"
     case Array(_, "jest", y) => s"Dlaczego $y?"
     case Array(_, "mogę", x) => s"Po co chcesz $x?"
     case Array(_, _, "imię") => "Jadwiga"
@@ -71,13 +77,16 @@ object Jadwiga {
     case Array(_, _, "dobrze") => "To super!"
     case Array(_, _, "mną") => "Jak długo zechcesz"
     case Array(_, _, "też") => "spoko"
-    case Array(_, _, x) => s"Często masz w głowie $x?"
+    case Array(_, _, "tobie") => "Nie czas teraz na to"
+    case Array(_, _, x) => s"Zastanawia mnie to \"$x\"... Rozwiniesz?"
 
     case Array("wiesz", _, _, _) => "Wiem ale chcę o tym usłyszeć od Ciebie."
     case Array("bo", _, _, _) => "Rozumiem"
     case Array("nie", x, y, z) => s"$y $z?"
     case Array("co", _, _, "słychać") => "to zależy"
+    case Array("jak", "ci", z, p) => s"A Tobie jak $z $p?"
     case Array(_, "matka", _, _) => "Opowiedz mi więcej o swojej rodzinie"
+    case Array(x, "mi", _, _) => s"Wolę jak Ty to robisz"
     case Array(x, "jest", y, z) => s"Dlaczego uważasz, że $x jest $y $z?"
     case Array(_, "lubisz", x, _) => s"Nie. Nie lubię $x"
     case Array(_, y, "jest", z) => s"Dlaczego $y jest $z?"
@@ -85,7 +94,10 @@ object Jadwiga {
     case Array(_, _, _, "imię") => "Jadwiga"
     case Array(_, _, _, x) => s"$x? Dlaczego tak uważasz?"
 
+    case Array("czy", "ty", _, _, _) => "Wolę rozmawiać o Tobie"
     case Array(_, _, _, _, _) => s"Naprawdę???"
+
+    case Array(_, _, _, _, x, _) => s"Jakie śmieszne słowo - \"$x\"! :D"
 
     case _ => "mów dalej"
   }
